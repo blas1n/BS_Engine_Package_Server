dbb3b1c037d5b03aef91fc5e30ebae4eb1995fc5
blAs1N <qazasa123@gmail.com>
2019-12-28 11:31:02 +0900

    Removed submodule external/TCP_Server_Library

--- server/main.cpp
+++ server/main.cpp
@@ -7,18 +7,22 @@ struct Protocol
 	char[123]
 };
 
-void OnAccept(Socket socket, std::byte* buffer)
+void OnAccept(Socket socket, const std::byte* buffer)
 {
+	char name[123];
+
 	if (!std::to_integer<bool>(buffer[0])) {
-		buffer <<= 1;
+		strncpy(name, reinterpret_cast<const char*>(buffer) + 1, 122);
+		std::ifstream in{ name };
+		
+		if (!in.is_open())
+			socket.Send("1");
 	}
 }
 
 int main()
 {
 	TCP tcp{ "config.ini" };
-	tcp.GetBufferSize();
-
 	tcp.SetOnAccept(OnAccept);
 	tcp.Run();
 	return 0;
