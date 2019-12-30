ef00d057eb5e5a43437375d385d259052887bd3e
blAs1N <qazasa123@gmail.com>
2019-12-30 09:01:18 +0900

    Some refactoring

--- server/main.cpp
+++ server/main.cpp
@@ -1,4 +1,5 @@
 #include "TCP.h"
+#include <bitset>
 #include <fstream>
 
 constexpr auto BUFFER_SIZE = 1024;
@@ -12,67 +13,64 @@ enum class State : ServWork::byte
 
 void SetState(ServWork::Buffer& buf, State state)
 {
-	switch (state)
+	std::bitset<128> bitSet = static_cast<ServWork::byte>(state);
+	std::string binaryStr = bitSet.to_string();
+	buf.Set(0, binaryStr.c_str());
+}
+
+void GetFile(ServWork::Socket& socket, const std::string& name, ServWork::Buffer& buf)
+{
+	std::ifstream in{ name.c_str(), std::ios::binary };
+
+	if (!in.is_open())
 	{
-	case State::Normal:
-		buf.Set(0, "00");
-		break;
+		SetState(buf, State::Exception);
+		buf.Set(2, "File not found.");
+		socket.Send(buf);
+		return;
+	}
 
-	case State::Exit:
-		buf.Set(0, "01");
-		break;
+	char tmp[BUFFER_SIZE - 2];
 
-	case State::Exception:
-		buf.Set(0, "10");
-		break;
+	while (!in.eof())
+	{
+		SetState(buf, State::Normal);
+		in.read(tmp, BUFFER_SIZE - 3);
+		buf.Set(2, tmp);
+		socket.Send(buf);
+		buf.Init();
+	}
+
+	SetState(buf, State::Exit);
+	socket.Send(buf);
+}
+
+void SetFile(ServWork::Socket& socket, const std::string& name, ServWork::Buffer& buf)
+{
+	std::ofstream out{ name.c_str(), std::ios::binary };
+
+	while (true)
+	{
+		int len = socket.Recv(buf);
+		out << buf;
+		if (len < 1) break;
+		buf.Init();
 	}
 }
 
 void OnAccept(ServWork::Socket socket)
 {
-	char tmp[BUFFER_SIZE - 2];
 	ServWork::Buffer buf{ BUFFER_SIZE };
 	socket.Recv(buf);
 
+	auto id = buf[0];
 	auto name = std::string{ buf.Get(1) };
 	buf.Init();
 
-	if (buf[0])
-	{
-		std::ifstream in{ name.c_str(), std::ios::binary };
+	using Func = void(*)(ServWork::Socket&, const std::string&, ServWork::Buffer&);
+	Func func[] = { GetFile, SetFile };
 
-		if (!in.is_open())
-		{
-			SetState(buf, State::Exception);
-			buf.Set(2, "File not found.");
-			socket.Send(buf);
-			return;
-		}
-
-		while (!in.eof())
-		{
-			SetState(buf, State::Normal);
-			in.read(tmp, BUFFER_SIZE - 3);
-			buf.Set(2, tmp);
-			socket.Send(buf);
-			buf.Init();
-		}
-
-		SetState(buf, State::Exit);
-		socket.Send(buf);
-	}
-	else
-	{
-		std::ofstream out{ name.c_str(), std::ios::binary };
-
-		while (true)
-		{
-			int len = socket.Recv(buf);
-			out << buf;
-			if (len < 1) break;
-			buf.Init();
-		}
-	}
+	func[id](socket, name, buf);
 }
 
 int main()
